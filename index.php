9450e8cd95c8e1dce63c8f4018131bdcb307c816
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-01-23 16:45:21 -0500

    changed quotes on the homepage and wrote the route in index.php

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Epadilla
+ * User: Raine Padilla
  * Date: 1/23/2018
- * Time: 9:10 AM
+ * Description: The fat-free "controller" or routing page for the dating website
  */
+require_once ('vendor/autoload.php');
+
+// Create the fat-free base instance and set debug level for development
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /', function ()
+{
+    $view = new View();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
